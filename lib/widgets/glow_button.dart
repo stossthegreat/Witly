1121b0eb2837e99e0398701d16969bdac50dc2e2
stossthegreat <daazawaaza@proton.me>
2025-07-13 23:57:37 +0100

    Create glow_button.dart

---
+++ lib/widgets/glow_button.dart
@@ -0,0 +1,47 @@
+        return Transform.scale(
+          scale: _scaleAnimation.value,
+          child: GestureDetector(
+            onTapDown: (_) => _controller.forward(),
+            onTapUp: (_) => _controller.reverse(),
+            onTapCancel: () => _controller.reverse(),
+            onTap: widget.disabled ? null : widget.onTap,
+            child: Container(
+              width: widget.width,
+              height: widget.height ?? 50,
+              decoration: _getDecoration(),
+              child: Material(
+                color: Colors.transparent,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 12,
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      if (widget.icon != null) ...[
+                        widget.icon!,
+                        const SizedBox(width: 8),
+                      ],
+                      Text(
+                        widget.text,
+                        style: TextStyle(
+                          color: widget.disabled 
+                              ? Colors.grey 
+                              : AppConstants.textPrimary,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
